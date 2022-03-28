6ff0c8d006fd05399235b9fb2704732e79bdbbdc
Abdullah Dinler <71527576+abdullahdinler@users.noreply.github.com>
2022-03-28 14:40:50 +0300

    Add project files.

---
+++ Sayisal_Loto_App/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿
+namespace Sayisal_Loto_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(178, 55);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(27, 32);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(211, 55);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(27, 32);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(277, 55);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(27, 32);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(244, 55);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(27, 32);
+            this.textBox4.TabIndex = 2;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(343, 55);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(27, 32);
+            this.textBox5.TabIndex = 5;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(310, 55);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(27, 32);
+            this.textBox6.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(178, 148);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(192, 34);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Sorgulla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Rockwell", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(194, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(155, 30);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Sayısal Loto";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ItemHeight = 23;
+            this.listBox1.Location = new System.Drawing.Point(178, 102);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(192, 27);
+            this.listBox1.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(137, 23);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Girilen Sayilar:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(149, 23);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Kazanan Sayilar:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(397, 14);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(271, 168);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(695, 203);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Sayisal Loto";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Sayisal_Loto_App/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sayisal_Loto_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int s1, s2, s3, s4, s5, s6, n1, n2, n3, n4, n5, n6;
+            s1 = rnd.Next(1, 91);
+            s2 = rnd.Next(1, 91);
+            s3 = rnd.Next(1, 91);
+            s4 = rnd.Next(1, 91);
+            s5 = rnd.Next(1, 91);
+            s6 = rnd.Next(1, 91);
+            n1 = Convert.ToInt16(textBox1.Text);
+            n2 = Convert.ToInt16(textBox1.Text);
+            n3 = Convert.ToInt16(textBox1.Text);
+            n4 = Convert.ToInt16(textBox1.Text);
+            n5 = Convert.ToInt16(textBox1.Text);
+            n6 = Convert.ToInt16(textBox1.Text);
+
+            
+            if (s1 == n1 && s2 == n2 && s3 == n3 && s4 == n4 && s5 == n5 && s6 == n6) 
+            {
+                textBox1.BackColor = Color.Green;
+                textBox2.BackColor = Color.Green;
+                textBox3.BackColor = Color.Green;
+                textBox4.BackColor = Color.Green;
+                textBox5.BackColor = Color.Green;
+                textBox6.BackColor = Color.Green;
+                listBox1.Items.Add(s1 + " " + s2 + " " + s3 + " " + s4 + " " + s5 + " " + s6);
+                MessageBox.Show("Tebrikler 1 Milyon TL Kazandınız.", "Sayısal Loto", MessageBoxButtons.OK,
+                    MessageBoxIcon.Asterisk);
+            }
+            else if (s1 == n1 && s2 == n2 && s3 == n3 && s4 == n4)
+            {
+                textBox1.BackColor = Color.Green;
+                textBox2.BackColor = Color.Green;
+                textBox3.BackColor = Color.Green;
+                textBox4.BackColor = Color.Green;
+                listBox1.Items.Add(s1 + " " + s2 + " " + s3 + " " + s4 + " " + s5 + " " + s6);
+                MessageBox.Show("Tebrikler 500 Bin TL Kazandınız.", "Sayısal Loto", MessageBoxButtons.OK,
+                    MessageBoxIcon.Asterisk);
+
+            }
+            else if (s1 == n1 && s2 == n2)
+            {
+                textBox1.BackColor = Color.Green;
+                textBox2.BackColor = Color.Green;
+                listBox1.Items.Add(s1 + " " + s2 + " " + s3 + " " + s4 + " " + s5 + " " + s6);
+                MessageBox.Show("Tebrikler 250 Bin TL Kazandınız.", "Sayısal Loto", MessageBoxButtons.OK,
+                    MessageBoxIcon.Asterisk);
+            }
+            else
+            {
+                textBox1.BackColor = Color.Red;
+                textBox2.BackColor = Color.Red;
+                textBox3.BackColor = Color.Red;
+                textBox4.BackColor = Color.Red;
+                textBox5.BackColor = Color.Red;
+                textBox6.BackColor = Color.Red;
+                listBox1.Items.Add(s1 + " " + s2 + " " + s3 + " " + s4 + " " + s5 + " " + s6);
+                MessageBox.Show("Üzgünüm Kazanamadınız!!!", "Sayısal Loto", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+    }
+}
